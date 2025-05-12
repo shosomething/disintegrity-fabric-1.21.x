e5b7332480ca564f070eba5d4cd7e15a490850bc
shosomething <5h0pp25267@gmail.com>
2025-05-12 16:15:43 +0200

    added nadium_crystal

--- src/main/java/net/shosomething/disintegrity/Disintegrity.java
+++ src/main/java/net/shosomething/disintegrity/Disintegrity.java
@@ -2,6 +2,7 @@ package net.shosomething.disintegrity;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.shosomething.disintegrity.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,8 @@ public class Disintegrity implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 
 	}
 }
---
+++ src/main/java/net/shosomething/disintegrity/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.shosomething.disintegrity.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.shosomething.disintegrity.Disintegrity;
+
+public class ModItems {
+    //ITEMS!
+    public static final Item NADIUM_CRYSTAL = registerItem("nadium_crystal", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Disintegrity.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Disintegrity.LOGGER.info("Registering moditems ("+Disintegrity.MOD_ID+")...");
+
+        //ingredients
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(NADIUM_CRYSTAL);
+        });
+    }
+
+}
