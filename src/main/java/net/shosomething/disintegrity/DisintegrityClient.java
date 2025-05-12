3767de98a9a98e6bbf887cb486ea41540372d78e
shosomething <5h0pp25267@gmail.com>
2025-05-12 15:38:03 +0200

    Initial Commit

---
+++ src/main/java/net/shosomething/disintegrity/DisintegrityClient.java
@@ -0,0 +1,10 @@
+package net.shosomething.disintegrity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DisintegrityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/shosomething/disintegrity/DisintegrityDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shosomething.disintegrity;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DisintegrityDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
